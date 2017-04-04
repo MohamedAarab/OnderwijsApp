8fcb0d1520c4626835e0636ae66397b454e658db
Mohamed Aarab <mohamed_aarab@live.nl>
2017-04-04 18:00:29 +0100

    Fixed data objects

--- src/app/cursus/cursus.component.ts
+++ src/app/cursus/cursus.component.ts
@@ -12,58 +12,67 @@ export class CursussenComponent{
     @Input() courses: Array<Object>;
     @Output() onSelectedCourse = new EventEmitter<Object>();
     courses : Array<Object>;
+    loading : boolean;
+    naam : string;
     currentCourse = <any>{};
 
     constructor(private cursusService: CursusService){
-        this.cursusService.getCursussen().subscribe(cursussen => {
-            this.courses = cursussen;
-            this.currentCourse = this.courses[0];
+      this.loading = true;
+    }
+
+    ngOnInit() {
+      this.cursusService.getCursussen().subscribe(cursussen => {
+          this.courses = cursussen;
+          console.log(this.courses);
+          this.currentCourse = this.courses[0];
+          for(let j = 0; j < this.courses.length; j++) {
+            //console.log(this.courses[j].leerdoelen);
+            for(let i = 0; i < this.courses[j].leerdoelen.length; i++){
+              this.cursusService.getDataByHref(this.courses[j].leerdoelen[i].href)
+                .subscribe(leerdoelen => {
+
+                  this.courses[j].leerdoelen[i].data = leerdoelen;
+                    if(this.courses[j].leerdoelen[i].data.bloomniveau != null) {
+                      this.cursusService.getDataByHref(this.courses[j].leerdoelen[i].data.bloomniveau.href)
+                        .subscribe(bloomniveau => {
+                          this.courses[j].leerdoelen[i].data.bloomniveau.data = bloomniveau;
+                        });
+                    }
+                  },
+                  error => console.log("Error: ", error));
+            }
+
+            for(let i = 0; i < this.courses[j].eindBT.length; i++){
+              // console.log(i);
+              // console.log(i);
+              this.cursusService.getDataByHref(this.courses[j].eindBT[i].href)
+                .subscribe(beroepstaken => {
+                    //console.log(i);
+                    this.courses[j].eindBT[i].data = beroepstaken;
+                  },
+                  error => console.log("Error: ", error));
+            }
+
+            for(let i = 0; i < this.courses[j].eindPS.length; i++){
+              this.cursusService.getDataByHref(this.courses[j].eindPS[i].href)
+                .subscribe(professionalskills => {
+                  this.courses[j].eindPS[i].data = professionalskills;
+                });
+            }
+
+            this.cursusService.getDataByHref(this.courses[j].coordinator.href)
+              .subscribe(coordinator => {
+                this.courses[j].coordinator.data = coordinator;
+              });
+          }
         },
         error => console.log("Error: ", error),
         () => {
-
-            for(var i = 0; i < this.currentCourse.leerdoelen.length; i++){
-                this.cursusService.getDataByHref(this.currentCourse.leerdoelen[i].href)
-                    .subscribe(leerdoelen => {
-                        this.currentCourse.leerdoelen[i - 1].data = leerdoelen;
-                },
-                error => console.log("Error: ", error),
-                () => {
-                    this.cursusService.getDataByHref(this.currentCourse.leerdoelen[i - 1].data.bloomniveau.href)
-                        .subscribe(bloomniveau => {
-                            this.currentCourse.leerdoelen[i - 1].data.bloomniveau.data = bloomniveau;
-                    });
-                });
-            }
-
-            // for(var i = 0; i < this.currentCourse.eindBT.length; i++){
-            //     // console.log(i);
-            //     console.log(i);
-            //     this.cursusService.getDataByHref(this.currentCourse.eindBT[i].href)
-            //         .subscribe(beroepstaken => {
-            //             this.currentCourse.eindBT[i - 1].data = beroepstaken;
-            //             console.log(this.currentCourse.eindBT);
-            //     },
-            //     error => console.log("Error: ", error),
-            //     () => {
-            //         i++;
-            //     });
-            //     continue;
-            // }
-
-            for(var i = 0; i < this.currentCourse.eindPS.length; i++){
-                this.cursusService.getDataByHref(this.currentCourse.eindPS[i].href)
-                    .subscribe(professionalskills => {
-                        this.currentCourse.eindPS[i - 1].data = professionalskills;
-                });
-            }
-
-            this.cursusService.getDataByHref(this.currentCourse.coordinator.href)
-                .subscribe(coordinator => {
-                    this.currentCourse.coordinator.data = coordinator;
-            });
-            console.log(this.currentCourse);
+          this.loading = false;
+          console.log(this.currentCourse);
+          //console.log(this.currentCourse.eindBT[0].data);
         });
+
     }
 
     onSelect(cour:Object) {
